1ee6e0ee36c4b374232d028c5fee9117904368b1
Nahuel Quiroga <nahuelq.78@gmail.com>
2020-07-23 19:26:22 -0300

    modif

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import check
 
 class Test_modules(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(check.sum(5, 7), 12)
+        self.assertEqual(check.sum(5, 7), 11)
 
 
 if __name__ == "__main__":
